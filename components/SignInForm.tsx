ad6fbdcba2356f44cbe3ff0e852945abd3a24b82
bechirMakhlouf <bechirM@proton.me>
2024-01-24 13:43:23 +0100

    fix password input on sign in form

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -12,12 +12,14 @@ import {
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { valibotResolver } from "@hookform/resolvers/valibot";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 const formSchema = v.object({
   email: v.string([v.email()]),
   password: v.string([
+    // v.empt
     v.minLength(8, "Your password must be at least 8 characters long"),
+    v.maxLength(64),
     v.regex(/[A-Z]/, "Your password must contain a uppercase letter."),
     v.regex(/[a-z]/, "Your password must contain a lowercase letter."),
     v.regex(/[0-9]/, "Your password must contain a number"),
@@ -29,60 +31,60 @@ type formInputs = v.Output<typeof formSchema>;
 const SignInForm = () => {
   const form = useForm<formInputs>({
     resolver: valibotResolver(formSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
+    // defaultValues: {
+    //   email: "hello darkness",
+    //   password: "my old friend",
+    // },
   });
   function onSubmit(values: formInputs) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
   return (
     <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="email"
-
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="type email..." {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your email.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="type password..." {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    account password
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <Button variant="outline" type="submit">Submit</Button>
-        </form>
-      </Form>
+      <div className="h-screen flex justify-center items-center">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="type email..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="type password..."
+                        autoComplete=""
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <Button variant="outline" type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
     </>
   );
 };
